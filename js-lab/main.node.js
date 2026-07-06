4635d86fb2d1d99cbede455a28b93170144c2f97
Michira <gaterudemichira@gmail.com>
2026-07-06 16:57:47 +0300

    js setup day 1

---
+++ js-lab/main.node.js
@@ -0,0 +1,9 @@
+console.log("Hello from JavaScript");
+const name = "Your Name";
+console.log(`Hi, my name is ${name}`);
+let year = 2026;
+year = year + 1;
+console.log(`Next year will be ${year}`);
+const marathon = { weeks: 30, phase: "foundations" };
+console.log(marathon);
+console.log(typeof marathon);
